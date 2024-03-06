33a0216200bd3b4da55221f660e8307c30f983b8
alishba-vaival <alishba.siddique@vaivaltech.com>
2024-03-06 10:50:40 +0500

    change id names, add useNavigate and onclick on button to navigate to other pages by clicking on button

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 import {
   Col,
   Container,
@@ -19,8 +21,67 @@ import dashboardIcon from "../../assets/images/figma/dashboard.svg";
 import arrowUp from "../../assets/images/figma/arrowUp.svg";
 import './Homepage.scss';
 
+const useHandleCashExpenseClick = () => {
+  const navigate = useNavigate();
+  const handleCashExpenseClick = () => {
+    navigate("/cashExpense");
+  };
+
+  return handleCashExpenseClick;
+};
+const useHandleBankExpenseClick = () => {
+  const navigate = useNavigate();
+  const handleBankExpenseClick = () => {
+    navigate("/bankExpense");
+  };
+
+  return handleBankExpenseClick;
+};
+
+const useHandleReceiptClick = () => {
+  const navigate = useNavigate();
+  const handleReceiptClick = () => {
+    navigate("/receipt");
+  };
+
+  return handleReceiptClick;
+};
+
+const useHandleCreateVoucherClick = () => {
+  const navigate = useNavigate();
+  const handleCreateVoucherClick = () => {
+    navigate("/createVoucher");
+  };
+
+  return handleCreateVoucherClick;
+};
+
+const useHandleOtherClick = () => {
+  const navigate = useNavigate();
+  const handleOtherClick = () => {
+    navigate("/other");
+  };
+
+  return handleOtherClick;
+};
+
+const useHandleViewsClick = () => {
+  const navigate = useNavigate();
+  const handleViewsClick = () => {
+    navigate("/views");
+  };
+
+  return handleViewsClick;
+};
+
 const HomePage = () => {
-  document.title = "CRM | Vaival Solutions";
+  document.title = "Fiscal Connect | Vaival Solutions";
+  const handleCashExpenseClick = useHandleCashExpenseClick();
+  const handleBankExpenseClick = useHandleBankExpenseClick();
+  const handleReceiptClick = useHandleReceiptClick();
+  const handleCreateVoucherClick = useHandleCreateVoucherClick();
+  const handleOtherClick = useHandleOtherClick();
+  const handleViewsClick = useHandleViewsClick();
   return (
     <React.Fragment>
       <div className="page-content">
@@ -28,78 +89,78 @@ const HomePage = () => {
         <Container fluid className="homeContainer">
         
           <Row>
-            {/* <Col xs={12}></Col> */}
+            <Col xs={12}></Col>
             <Col md="2">
-              <Card body className="cardHome">
-                <CardTitle tag="h5" className="cardTitleHome"
+              <Card body className="homeCard">
+                <CardTitle tag="h5" className="homeCardTitle"
                   style={{
                     backgroundColor: "#F2F8FF",
                   }}
                 >
                   <img alt="CashExpense" src={cashExpense} width="100%" />
                 </CardTitle>
-                <Button className="btnHome">Cash Expense</Button>
+                <Button className="homeBtn"  onClick={handleCashExpenseClick}>Cash Expense</Button>
               </Card>
             </Col>
             <Col md="2">
-              <Card body className="cardHome">
-                <CardTitle tag="h5" className="cardTitleHome"
+              <Card body className="homeCard">
+                <CardTitle tag="h5" className="homeCardTitle"
                   style={{
                     backgroundColor: "#F2F8FF",
                   }}>
                   <img alt="BankExpense" src={bankExpense} width="100%" />
                 </CardTitle>
-                <Button className="btnHome">Bank Expense</Button>
+                <Button className="homeBtn" onClick={handleBankExpenseClick}>Bank Expense</Button>
               </Card>
             </Col>
             <Col md="2">
-              <Card body className="cardHome">
-                <CardTitle tag="h5" className="cardTitleHome"
+              <Card body className="homeCard">
+                <CardTitle tag="h5" className="homeCardTitle"
                 style={{
                     backgroundColor: "#F6F2FF",
                   }}
                 >
                   <img alt="receipt" src={receipt} width="100%" />
                 </CardTitle>
-                <Button className="btnHome">Receipt</Button>
+                <Button className="homeBtn" onClick={handleReceiptClick}>Receipt</Button>
               </Card>
             </Col>
             <Col md="2">
-              <Card body className="cardHome">
-                <CardTitle tag="h5" className="cardTitleHome"
+              <Card body className="homeCard">
+                <CardTitle tag="h5" className="homeCardTitle"
                   style={{
                     backgroundColor: "#F4FCFB",
                   }}
                 >
                   <img alt="other" src={other} width="100%" />
                 </CardTitle>
-                <Button className="btnHome">Other</Button>
+                <Button className="homeBtn" onClick={handleOtherClick}>Other</Button>
               </Card>
             </Col>
             </Row>
             <Row>
             <Col md="2">
-              <Card body className="cardHome">
-                <CardTitle tag="h5"className="cardTitleHome"
+              <Card body className="homeCard">
+                <CardTitle tag="h5"className="homeCardTitle"
                   style={{
                     backgroundColor: "#FFF9F2",
                   }}
                 >
                   <img alt="Views" src={views} width="100%" />
                 </CardTitle>
-                <Button className="btnHome">Views</Button>
+                <Button className="homeBtn" onClick={handleViewsClick}>Views</Button>
               </Card>
             </Col>
             <Col md="2">
-              <Card body className="cardHome">
-                <CardTitle tag="h5" className="cardTitleHome"
+              <Card body className="homeCard">
+                <CardTitle tag="h5" className="homeCardTitle"
                   style={{
                     backgroundColor: "#F2F8FF",
                   }}
                 >
                   <img alt="CreateVoucher" src={createVoucher} width="100%" />
                 </CardTitle>
-                <Button className="btnHome">Create Voucher</Button>
+                <Button className="homeBtn" onClick={handleCreateVoucherClick}>Create Voucher</Button>
               </Card>
             </Col>
           </Row>
