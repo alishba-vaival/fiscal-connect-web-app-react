d92941710662ab305adf97e6720cc24f2b4c35b2
kashif1304 <154964832+kashif1304@users.noreply.github.com>
2024-03-08 12:11:30 +0500

    make (initial) dashboard

--- src/store/dashboard/action.js
+++ src/store/dashboard/action.js
@@ -1,37 +1,44 @@
 import {
   API_RESPONSE_SUCCESS,
   API_RESPONSE_ERROR,
-  GET_BALANCEOVERVIEW_CHARTS_DATA,
-  GET_DIALTYPE_CHARTS_DATA,
-  GET_SALESFORECAST_CHARTS_DATA
+  GET_COUNTRY_CHARTS_DATA,
+  GET_AUDIENCESMETRICS_CHARTS_DATA,
+  GET_USERDEVICE_CHARTS_DATA,
+  GET_AUDIENCESSESSIONS_CHARTS_DATA
 } from "./actionType";
 
 // common success
-export const dashboardCrmApiSuccess = (actionType, data) => ({
+export const dashboardAnalyticsApiSuccess = (actionType, data) => ({
   type: API_RESPONSE_SUCCESS,
   payload: { actionType, data },
 });
 
 // common error
-export const dashboardCrmApiError = (actionType, error) => ({
+export const dashboardAnalyticsApiError = (actionType, error) => ({
   type: API_RESPONSE_ERROR,
   payload: { actionType, error },
 });
 
-// Balance Overview charts data
-export const getBalanceOverviewChartsData = (balanceOverviewData) => ({
-  type: GET_BALANCEOVERVIEW_CHARTS_DATA,
-  payload: balanceOverviewData
+// charts data
+export const getCountryChartsData = (chartData) => ({
+  type: GET_COUNTRY_CHARTS_DATA,
+  payload: chartData
 });
 
-// Dial Type charts data
-export const getDialTypeChartsData = (dialTypeData) => ({
-  type: GET_DIALTYPE_CHARTS_DATA,
-  payload: dialTypeData
+// Audiences Metrics
+export const getAudiencesMetricsChartsData = (audiencesMetricsData) => ({
+  type: GET_AUDIENCESMETRICS_CHARTS_DATA,
+  payload: audiencesMetricsData
 });
 
-// Sales Forecast charts data
-export const getSalesForecastChartsData = (salesForecastData) => ({
-  type: GET_SALESFORECAST_CHARTS_DATA,
-  payload: salesForecastData
+// Users by Device
+export const getUserDeviceChartsData = (userDeviceData) => ({
+  type: GET_USERDEVICE_CHARTS_DATA,
+  payload: userDeviceData
+});
+
+// Audiences Sessions by Country
+export const getAudiencesSessionsChartsData = (audiencesSessionsData) => ({
+  type: GET_AUDIENCESSESSIONS_CHARTS_DATA,
+  payload: audiencesSessionsData
 });
